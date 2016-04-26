882f783fbc74920a5307d11e380ca0c9bfffe92b
Crypt0s <bryanhalf@gmail.com>
2016-04-26 18:55:16 -0400

    Add comment about gnuradio's implementation of FM mod

--- sine.js
+++ sine.js
@@ -24,7 +24,31 @@ no, because we would still need to track state across the 2 samples == better to
 aha ok so we will simply build a t-based function which outputs deviations to freq value at set intervals!
 
 
+here is GNURadio's implementation of a Frequency Modulator:
+      for(int i = 0; i < noutput_items; i++) {
+        d_phase = d_phase + d_sensitivity * in[i];
+
+        //place phase in [-pi, +pi[
+        #define F_PI ((float)(M_PI))
+        d_phase   = std::fmod(d_phase + F_PI, 2.0f * F_PI) - F_PI;
+
+        float oi, oq;
+
+        int32_t angle = gr::fxpt::float_to_fixed (d_phase);
+        gr::fxpt::sincos(angle, &oq, &oi);
+        out[i] = gr_complex(oi, oq);
+      }
+
+      return noutput_items;
+
+
+
 */
+
+
+
+
+
 function modsine(freq, time, samp_freq, amplitude, phase, yshift){
   freq = 2 * 100
   time = 5
@@ -50,10 +74,12 @@ function simplesine(freq, time, samp_freq, amplitude, phase, yshift){
   var points = []
   for(t = 0; t < time; t += (1.0/samp_freq)){
     var x = t
+    var z = t % 2
     var y = yshift + (amplitude * Math.sin(2 * Math.PI * (1/(freq)) * t + phase))
+    console.log('(' + x + ',' + y + ')' + ' : ' + z)
     points.push([x,y])
   }
-  console.log(points.length)
+  console.log(points)
   return points
 }
 
